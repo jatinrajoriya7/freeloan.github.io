2cc22f83b027a7cded39b0ccdc5b849da8e8acf5
jatinrajoriya7 <85305744+jatinrajoriya7@users.noreply.github.com>
2021-06-04 09:50:18 -0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
 
-$dbhost = "";
-$dbuser = "";
-$dbpass = "";
-$dbname = "";
+$dbhost = "sql6.freemysqlhosting.net";
+$dbuser = "sql6416738sql6416738";
+$dbpass = "sql6416738";
+$dbname = "sql6416738";
 
 if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
 {
